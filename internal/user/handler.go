c8d70bd09bd56960c9fbbc4146a9c32443b4a3a6
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-07 12:06:15 +0300

    added test and fix header write

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -30,21 +30,31 @@ func (h *handler) Register(router *httprouter.Router) {
 }
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("this is list of users"))
 }
 
 func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte("this is create user"))
 }
+
 func (h *handler) GetUserByUuID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("this is get user"))
 }
+
 func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("this is update user"))
 }
+
 func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("this is partially update user"))
 }
+
 func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(http.StatusNoContent)
 	w.Write([]byte("this is delete user"))
 }
