fbdd6859b2395b914823d7614b0805d5df79912d
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-07 11:33:40 +0300

    rest methods stub

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -5,13 +5,39 @@ import (
 	"net/http"
 )
 
+const (
+	usersUrl = "/users"
+	userUrl  = "/users/:uuid"
+)
+
 type handler struct {
 }
 
 func (h *handler) Register(router *httprouter.Router) {
-	router.GET("/users", h.GetList)
+	router.GET(usersUrl, h.GetList)
+	router.POST(userUrl, h.CreateUser)
+	router.GET(userUrl, h.GetUserByUuID)
+	router.PUT(userUrl, h.UpdateUser)
+	router.PATCH(userUrl, h.PartiallyUpdateUser)
+	router.DELETE(userUrl, h.DeleteUser)
 }
 
 func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 	w.Write([]byte("this is list of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is create user"))
+}
+func (h *handler) GetUserByUuID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is get user"))
+}
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is update user"))
+}
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is partially update user"))
+}
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Write([]byte("this is delete user"))
 }
