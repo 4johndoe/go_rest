b5bb01b33be12786932c4fe93be87e67bcc5bcf0
bogdan demchenko <bogdandemchenko@gmail.com>
2022-05-07 10:42:28 +0300

    added httprouter

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func main() {
+	router := httprouter.New()
+	router.GET("/", IndexHandler)
 }
